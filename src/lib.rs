78c9d9f2b7d8cf6cea3a77fbe3c64688f1821117
Peter Lyons Kehl <peter.kehl@gmail.com>
2026-04-11 21:41:36 -0700

    use proc_macro2::Span::call_site(); minor

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ use proc_macro::TokenStream;
 use proc_macro_rules::rules;
 use quote::quote_spanned;
 use syn::spanned::Spanned;
+use proc_macro2::Span;
 
 #[cfg(not(debug_assertions))]
 compile_error!("If you use prudent-macros-lint (usually through feature 'lint_unused_unsafe' of prudent crate), use it in debug build only.");
@@ -12,8 +13,16 @@ compile_error!("If you use prudent-macros-lint (usually through feature 'lint_un
 pub fn unsafe_fn(input: TokenStream) -> TokenStream {
     rules!(input.into() => {
         ( $f:expr ) => {
+            // Either of the following is fine: f.span() or Span::call_site()
+            //
+            // let span = f.span();
+            let span = Span::call_site();
 
-            let span = f.span();
+            // We HAVE TO use `quote::quote_spanned`. If we used `quote::quote` instead, any
+            // `#[deny(unused_unsafe)]` or `#[forbid(unused_unsafe)]` on the user's side would have
+            // no effect - the lint would not trigger, even though `quote::quote` docs say that it
+            // uses caller site span for tokens written in it.
+
             // @TODO Simplify once https://github.com/rust-lang/rust/issues/15701
             // `#![feature(stmt_expr_attributes)]` is stable
             //
